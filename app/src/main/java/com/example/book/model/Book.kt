4423d926ac3e5d008b17add599614dd3f8fe4b6e
Mateus Morais <85200990+merfeus@users.noreply.github.com>
2021-09-04 16:04:52 -0300

    BOOK-5: Created Models

--- app/src/main/java/com/example/book/model/Book.kt
+++ app/src/main/java/com/example/book/model/Book.kt
@@ -1,5 +1,41 @@
 package com.example.book.model
 
+import com.google.gson.annotations.SerializedName
+
+data class BookResponse(
+
+    @SerializedName("items")
+    val items : List<Book>
+)
+
 data class Book(
-    val id: String
+
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("volumeInfo")
+    val volumeInfo: VolumeInfo,
+    @SerializedName("publishedDate")
+    val publishedDate: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("pageCount")
+    val pageCount: Int,
+    @SerializedName("imageLinks")
+    val imageLinks: ImageLinks?
+
+)
+data class VolumeInfo(
+
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("authors")
+    val authors: List<String>
+)
+
+data class ImageLinks(
+
+    @SerializedName("smallThumbnail")
+    val smallThumbnail: String?,
+    @SerializedName("thumbnail")
+    val thumbnail: String?
 )
