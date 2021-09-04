d43bf2e6bbde0a56491985bc2d71afe002ce7032
leo-vieiraa <87448838+leo-vieiraa@users.noreply.github.com>
2021-09-04 18:17:34 -0300

    BOOK-11: Added ViewModel

--- app/src/main/java/com/example/book/viewmodel/BookListingViewModel.kt
+++ app/src/main/java/com/example/book/viewmodel/BookListingViewModel.kt
@@ -1,7 +1,39 @@
 package com.example.book.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.book.model.Book
+import com.example.book.repository.BooksRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class BookListingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class BookListingViewModel @Inject constructor(
+
+    private val repository: BooksRepository
+
+) : ViewModel() {
+
+
+    private val _books = MutableLiveData<List<Book>>()
+    val books: LiveData<List<Book>> = _books
+
+    val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+    fun getBooksByTerms(terms: String) {
+
+        repository.getBooksByTerms(terms) { response, error ->
+
+            response?.let {
+                _books.value = it
+            }
+            error?.let {
+                _error.value = it
+            }
+
+        }
+    }
+
 }
