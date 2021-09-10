a6b95e7802c2a2c1f8a82f6b8c47e913bf47c593
leo-vieiraa <87448838+leo-vieiraa@users.noreply.github.com>
2021-09-10 09:01:57 -0300

    BOOK-16: Added book details section

---
+++ app/src/main/java/com/example/book/view/fragments/BookDetailsFragment.kt
@@ -0,0 +1,53 @@
+package com.example.book.view.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.example.book.R
+import com.example.book.databinding.BookDetailsFragmentBinding
+import com.example.book.model.Book
+import com.example.book.viewmodel.BookDetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BookDetailsFragment : Fragment(R.layout.book_details_fragment) {
+
+    companion object {
+        fun newInstance(id: String): BookDetailsFragment {
+            return BookDetailsFragment().apply {
+                arguments = Bundle().apply {
+                    putString("book_id", id)
+                }
+            }
+        }
+    }
+
+    private lateinit var viewModel: BookDetailsViewModel
+    private lateinit var binding: BookDetailsFragmentBinding
+    private val observerBook = Observer<Book?> {
+        bindData(it)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = BookDetailsFragmentBinding.bind(view)
+
+        viewModel = ViewModelProvider(this).get(BookDetailsViewModel::class.java)
+        viewModel.book.observe(viewLifecycleOwner, observerBook)
+
+        val bookId = arguments?.getString("book_id") as String
+
+        viewModel.getBookById(bookId)
+
+    }
+
+    private fun bindData(book: Book?) {
+
+        binding.bookNameTextView.text = book!!.id
+
+    }
+
+}
