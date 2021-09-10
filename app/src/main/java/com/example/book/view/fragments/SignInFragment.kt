beb3a12e03616d78b302c784b588ffa6bca9609a
Mateus Morais <85200990+merfeus@users.noreply.github.com>
2021-09-10 13:18:51 -0300

    BOOK-23: Created Layout Singin

---
+++ app/src/main/java/com/example/book/view/fragments/SignInFragment.kt
@@ -0,0 +1,33 @@
+package com.example.book.view.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.book.R
+import com.example.book.viewmodel.SignInViewModel
+
+class SignInFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SignInFragment()
+    }
+
+    private lateinit var viewModel: SignInViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.sign_in_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
