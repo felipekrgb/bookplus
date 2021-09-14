f1a77e664621af0f687cdc249fa541cc4d836ca2
felipekrgb <felipekrgb@gmail.com>
2021-09-14 09:25:43 -0300

    BOOK-29: Linking first screen with sign in screen

--- app/src/main/java/com/example/book/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/book/view/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.book.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.book.R
+import com.example.book.view.fragments.IntroductionFragment
 import com.example.book.view.fragments.SignInFragment
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, SignInFragment.newInstance())
+                .replace(R.id.container, IntroductionFragment())
                 .commitNow()
         }
     }
--- app/src/main/java/com/example/book/view/fragments/IntroductionFragment.kt
+++ app/src/main/java/com/example/book/view/fragments/IntroductionFragment.kt
@@ -6,10 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.book.R
+import com.example.book.databinding.IntroductionFragmentBinding
+import com.example.book.utils.replaceFragment
+import com.example.book.view.activities.MainActivity
 
 class IntroductionFragment : Fragment(R.layout.introduction_fragment) {
 
+    private lateinit var binding: IntroductionFragmentBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = IntroductionFragmentBinding.bind(view)
+
+        binding.singInButton.setOnClickListener {
+            (requireActivity() as MainActivity).replaceFragment(SignInFragment.newInstance())
+        }
+
+        // TODO adicionar SIGNUP
+        binding.signUpButton.setOnClickListener {
+            (requireActivity() as MainActivity).replaceFragment(SignInFragment.newInstance())
+        }
+
     }
 }
