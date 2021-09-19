1cc99c5291d420f98cb7be1f2ae4e4a75b5accfc
Mateus Morais <85200990+merfeus@users.noreply.github.com>
2021-09-19 17:05:06 -0300

    BOOK-32: Created CheckBox in Details Fragment and Add SwipeRefesh

--- app/src/main/java/com/example/book/view/fragments/BookFavoritesFragment.kt
+++ app/src/main/java/com/example/book/view/fragments/BookFavoritesFragment.kt
@@ -3,16 +3,17 @@ package com.example.book.view.fragments
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.book.R
 import com.example.book.adapter.BookFavoritesAdapter
 import com.example.book.databinding.BookFavoritesFragmentBinding
 import com.example.book.model.Book
 import com.example.book.view.activities.BookDetailsActivity
-import com.example.book.view.dialogs.BasicDetailsFragment
 import com.example.book.viewmodel.BookFavoritesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,6 +30,7 @@ class BookFavoritesFragment : Fragment(R.layout.book_favorites_fragment) {
             Intent(activity?.applicationContext, BookDetailsActivity::class.java)
         intentToDetails.putExtra("book", book)
         startActivity(intentToDetails)
+        swipeRefreshLayout.isRefreshing = false
     }
     private lateinit var binding: BookFavoritesFragmentBinding
 
@@ -45,14 +47,15 @@ class BookFavoritesFragment : Fragment(R.layout.book_favorites_fragment) {
         }
     }
 
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(BookFavoritesViewModel::class.java)
         binding = BookFavoritesFragmentBinding.bind(view)
-
         startRecyclerView()
         startViewModel()
-
+        startSwipe()
     }
 
     private fun startViewModel() {
@@ -62,8 +65,22 @@ class BookFavoritesFragment : Fragment(R.layout.book_favorites_fragment) {
     }
 
     private fun startRecyclerView() {
+        swipeRefreshLayout = binding.swipeContainer
         binding.recyclerViewFavs.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerViewFavs.adapter = adapter
+    }
+
+    private fun loadFavBook() {
+        swipeRefreshLayout.isRefreshing = false
+        viewModel.fetchAllBooksFav()
+    }
+
+    private fun startSwipe() {
+        swipeRefreshLayout.setOnRefreshListener {
+            Toast.makeText(requireContext(), "Página recarregada", Toast.LENGTH_LONG ).show()
+            swipeRefreshLayout.isRefreshing = false
+            loadFavBook()
+        }
     }
 
 }
