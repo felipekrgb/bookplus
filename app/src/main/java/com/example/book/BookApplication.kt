0036c97858d6dcc3278c957e9e791d83c7065cd4
Mateus Morais <85200990+merfeus@users.noreply.github.com>
2021-09-24 10:24:15 -0300

    BOOK-68: Created Notification Handler and WOrkMenager

--- app/src/main/java/com/example/book/BookApplication.kt
+++ app/src/main/java/com/example/book/BookApplication.kt
@@ -1,8 +1,82 @@
 package com.example.book
 
 import android.app.Application
+import androidx.core.app.NotificationManagerCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.work.*
+import com.example.book.service.notification.NotificationHandler
+import com.example.book.service.notification.NotificationWorkMenage
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import java.util.*
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BookApplication: Application() {
+class BookApplication : Application(), LifecycleObserver {
+
+    @Inject
+    lateinit var notificationHandler: NotificationHandler
+
+    override fun onCreate() {
+        super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppBackgrounded() {
+        CoroutineScope(Dispatchers.Main).async {
+            showNotification()
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onAppDestroy() {
+        CoroutineScope(Dispatchers.Main).async {
+            startWork()
+        }
+    }
+
+    suspend fun showNotification() {
+        delay(3000)
+        notificationHandler.createNotification(
+            "Como está sua leitura?",
+            "Muitos homens iniciaram uma nova era na sua vida a partir da leitura de um livro \uD83D\uDE09"
+        )
+            .let {
+                val notificationManager = NotificationManagerCompat.from(applicationContext)
+                notificationManager.notify(1, it)
+            }
+    }
+
+    suspend fun startWork() {
+        delay(2000)
+        WorkManager.getInstance(applicationContext).let {
+            val workManager = WorkManager.getInstance(applicationContext)
+
+            val constraints = Constraints.Builder()
+                .setRequiresCharging(false)
+                .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+                .setRequiresBatteryNotLow(true)
+                .build()
+
+            val periodicWorkRequest = PeriodicWorkRequestBuilder<NotificationWorkMenage>(
+                60,
+                TimeUnit.MINUTES
+            ).setConstraints(constraints).build()
+
+            workManager.enqueueUniquePeriodicWork(
+                UUID.randomUUID().toString(),
+                ExistingPeriodicWorkPolicy.KEEP,
+                periodicWorkRequest
+            )
+        }
+    }
 }
+
